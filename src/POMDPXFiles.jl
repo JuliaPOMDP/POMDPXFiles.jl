2075bcbcde1e246d86763d08bac31d1eb6c0d4b4
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2023-06-09 12:21:21 -0400

    EzXML wasn't imported correctly?

--- src/POMDPXFiles.jl
+++ src/POMDPXFiles.jl
@@ -5,6 +5,7 @@ using POMDPTools
 using ProgressMeter
 using Parameters
 
+import EzXML
 import EzXML: Node, XMLDocument, ElementNode, addelement!, link!, write, readxml, root, findfirst, findall, nodecontent, setroot!, prettyprint
 
 export
